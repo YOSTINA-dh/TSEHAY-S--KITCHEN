1eb7cf88ac61ae082b57fd23eb874d683c75ef2e
YOSTINA-dh <yostinadesta@gmail.com>
2021-04-16 23:19:25 +0000

    Add REgister template

--- app.py
+++ app.py
@@ -24,6 +24,11 @@ def get_lists():
     return render_template("lists.html", lists=lists)
 
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
