f4f3743d659e48ba0b0ed94628bc589a646c47f8
YOSTINA-dh <yostinadesta@gmail.com>
2021-04-16 22:22:54 +0000

    Add Materialize Navbar and sidenav

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,20 @@
-$(document).ready(function() {
-    //When the document is ready, the user is able to select a category before submiting the recipe
-    $('select').material_select();
-    //On smaller screens (mobile version) the user can use the menu by the side navigation
-    $(".button-collapse").sideNav();
-    //Feedback form modal
-    $('.modal').modal();
-    });
+/*
+    jQuery for MaterializeCSS initialization
+*/
+
+$(document).ready(function () {
+    $(".sidenav").sidenav({edge: "left"});
+});
+
+
+
+
+
+/*
+    vanilla JavaScript for MaterializeCSS initialization
+*/
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     let sidenavs = document.querySelectorAll(".sidenav");
+//     let sidenavsInstance = M.Sidenav.init(sidenavs, {edge: "right"});
+// });
