d3b5f03995952d7978d812ee8b5a46eadb14f212
YOSTINA-dh <yostinadesta@gmail.com>
2021-04-20 11:18:22 +0000

    Creat add_list template

--- app.py
+++ app.py
@@ -99,6 +99,11 @@ def logout():
     return redirect(url_for("login"))
 
 
+@app.route("/add_list")
+def add_list():
+    return render_template("add_list.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
